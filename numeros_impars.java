df18d8c37e460aeae3c57616fb110a3b424cadf5
marcos <mvsb2@discente.ifpe.edu.br>
2023-10-13 20:02:00 -0300

    update3

--- numeros_impars.java
+++ numeros_impars.java
@@ -1,10 +1,10 @@
 public class numeros_impars {
     public static void main(String[] args) {
-        
+
         int somar;
         for (int i = 0; i <= 100; i++) {
 
-           somar = i % 2;
+            somar = i % 2;
 
             if (somar == 1) {
                 System.out.println("o numero " + i + " é impar");
